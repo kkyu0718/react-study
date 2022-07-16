44eb630e0f6e3c1ac59ddefd953830f17563e786
kkyu0718 <net605@naver.com>
2022-07-17 02:39:21 +0900

    add drag and drop

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -25,7 +25,8 @@ export default function App() {
   
   
   
-  return(<div className='flex items-center justify-center w-screen h-screen bg-blue-100' >
+  return(
+  <div className='flex items-center justify-center w-screen h-screen bg-blue-100' >
       <div className='w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg'>
         <div className='flex justify-between mb-3'>
           <h1>오늘 할 일!</h1>
--- react-todo-app/src/components/List.js
+++ react-todo-app/src/components/List.js
@@ -1,50 +1,61 @@
-import React from 'react'
+import React from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import List from "./List";
 
-export default function List({todoData, setTodoData}) {
+export default function Lists({ todoData, setTodoData }) {
 
+  const handleEnd = (result) => {
+    // result 매개변수에는 source 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함됩니다.
+    console.log(result);
+    // 목적지가 없으면(이벤트 취소) 이 함수를 종료합니다.
+    if (!result.destination) return;
 
-    const handleCompleteChange = (id) => {
-        let newTodoData = todoData.map((data) => {
-          if (data.id === id){
-            data.completed = !data.completed;
-          }
-          return data;
-        })
-        setTodoData(newTodoData)
-    }
+    // 리액트 불변성을 지켜주기 위해 새로운 todoData 생성 
+    const newTodoData = todoData;
 
+    // 1. 변경시키는 아이템을 배열에서 지워줍니다.
+    // 2. return 값으로 지워진 아이템을 잡아줍니다.
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
 
+    // 원하는 자리에 reorderedItem을 insert 해줍니다.
+    newTodoData.splice(result.destination.index, 0, reorderedItem);
+    setTodoData(newTodoData);
+    localStorage.setItem("todoData", JSON.stringify(newTodoData));
+  };
 
-  
-
-    const handleClick = (id) =>{
-    let newTodoData = todoData.filter((data) => data.id !== id)
-    //console.log(this) // App
-    setTodoData(newTodoData)
-  }
-   
-                
-    return (
+  return (
     <div>
-      {todoData.map((data) =>(
-          <div key = {data.id}>
-            <div className='flex items-center justify-between w-full px-4 py-1 my-2 text-gray-500 bg-gray-100 border rounded'>
-            <div className = "items-center">
-              <input 
-            type = "checkbox" 
-            defaultChecked = {data.completed} 
-            onChange = {() => handleCompleteChange(data.id)}/>
-            {" "} 
-            <span className= {data.completed ? "line-through" : undefined}>{data.title}</span>
-            </div>
-            <div className='items-center'>
+      <DragDropContext onDragEnd={handleEnd}>
 
-            <button className = "px-4 py-2" onClick = {() => handleClick(data.id)}>X</button>
+        <Droppable droppableId="to-dos">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <List
+                      key={data.id}
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
             </div>
-            </div>
-          </div>
+          )}
+        </Droppable>
 
-))}
+      </DragDropContext>
     </div>
-  )
+  );
 }
