aec69f80ac92089d781a3e9f2ce433ce249b003c
kkyu0718 <net605@naver.com>
2022-07-19 15:12:58 +0900

    final commit

--- react-todo-app/src/components/List.js
+++ react-todo-app/src/components/List.js
@@ -1,61 +1,96 @@
-import React from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import List from "./List";
+import React, { useState } from 'react'
 
-export default function Lists({ todoData, setTodoData }) {
+const List = ({ id, title, completed, todoData, setTodoData, provided, snapshot }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
 
-  const handleEnd = (result) => {
-    // result 매개변수에는 source 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함됩니다.
-    console.log(result);
-    // 목적지가 없으면(이벤트 취소) 이 함수를 종료합니다.
-    if (!result.destination) return;
+    const handleClick = (id) => {
+        let newTodoData = todoData.filter((data) => data.id !== id);
+        setTodoData(newTodoData);
+        localStorage.setItem("todoData", JSON.stringify(newTodoData));
+    };
 
-    // 리액트 불변성을 지켜주기 위해 새로운 todoData 생성 
-    const newTodoData = todoData;
+    const handleCompleteChange = (id) => {
+        let newTodoData = todoData.map((data) => {
+            if (data.id === id) {
+                data.completed = !data.completed;
+            }
+            return data;
+        });
+        setTodoData(newTodoData);
+        localStorage.setItem("todoData", JSON.stringify(newTodoData));
+    };
 
-    // 1. 변경시키는 아이템을 배열에서 지워줍니다.
-    // 2. return 값으로 지워진 아이템을 잡아줍니다.
-    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+    const handleEditChange = (e) => {
+        setEditedTitle(e.target.value);
+    };
 
-    // 원하는 자리에 reorderedItem을 insert 해줍니다.
-    newTodoData.splice(result.destination.index, 0, reorderedItem);
-    setTodoData(newTodoData);
-    localStorage.setItem("todoData", JSON.stringify(newTodoData));
-  };
+    const handleSubmit = () => {
+        let newTodoData = todoData.map((data) => {
+            if (data.id === id) {
+                data.title = editedTitle;
+            }
+            return data;
+        });
+        setTodoData(newTodoData);
+        localStorage.setItem("todoData", JSON.stringify(newTodoData));
+        setIsEditing(false);
+    };
 
-  return (
-    <div>
-      <DragDropContext onDragEnd={handleEnd}>
-
-        <Droppable droppableId="to-dos">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {todoData.map((data, index) => (
-                <Draggable
-                  key={data.id}
-                  draggableId={data.id.toString()}
-                  index={index}
-                >
-                  {(provided, snapshot) => (
-                    <List
-                      key={data.id}
-                      id={data.id}
-                      title={data.title}
-                      completed={data.completed}
-                      todoData={todoData}
-                      setTodoData={setTodoData}
-                      provided={provided}
-                      snapshot={snapshot}
+    if (isEditing) {
+        return (
+            <div className="flex items-center justify-between w-full px-4 py-1 my-1 text-gray-600 bg-gray-100 border rounded row">
+                <form onSubmit={handleSubmit}>
+                    <input
+                        className="w-full px-3 py-2 mr-4 text-gray-500 appearance-none"
+                        value={editedTitle}
+                        onChange={handleEditChange}
+                        autoFocus
                     />
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
+                </form>
+                <div className="items-center">
+                    <button
+                        class="px-4 py-2 float-right"
+                        onClick={() => setIsEditing(false)}
+                        type="button"
+                    >
+                        x
+                    </button>
+                    <button onClick={handleSubmit} class="px-4 py-2 float-right"  type="submit">
+                        save
+                    </button>
+                </div>
             </div>
-          )}
-        </Droppable>
+        )
+    } else {
+        return (
+            <div
+                key={id}
+                {...provided.draggableProps}
+                ref={provided.innerRef}
+                {...provided.dragHandleProps}
+                className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}>
+                <div className="items-center">
+                    <input
+                        type="checkbox"
+                        onChange={() => handleCompleteChange(id)}
+                        defaultChecked={completed}
+                    />{" "}
+                    <span className={completed ? "line-through" : undefined}>{title}</span>
+                </div>
+                <div className="items-center">
+                    <button className="float-right px-4 py-2" onClick={() => handleClick(id)}>
+                        x
+                    </button>
+                    <button className="float-right px-4 py-2" onClick={() => setIsEditing(true)}>
+                        edit
+                    </button>
+                </div>
+            </div>
+        )
+    }
 
-      </DragDropContext>
-    </div>
-  );
 }
+
+export default List
+
