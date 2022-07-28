d42d144ffee5b2598318a6882ae0829e4e03d0ba
kkyu0718 <net605@naver.com>
2022-07-28 19:53:42 +0900

    이미지 슬라이더 / 필터 추가

--- react-shopping-mall/client/src/components/views/LandingPage/LandingPage.js
+++ react-shopping-mall/client/src/components/views/LandingPage/LandingPage.js
@@ -1,26 +1,120 @@
 import axios from 'axios';
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
+import {Icon, Col, Card, Row} from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import ImageSlider  from '../../utils/ImageSlider'
+import CheckBox from './Sections/CheckBox';
+import { continents } from './Sections/Datas';
+
 
 function LandingPage() {
+    const [Products, setProducts] = useState([])
+    const [Skip, setSkip] = useState(0)
+    const [Limit, setLimit] = useState(8)
+    const [PostSize, setPostSize] = useState(0)
+    const [Filters, setFilters] = useState({
+        continents : [],
+        price : []
+    })
 
     useEffect(() => {
+
+        let body = {
+            skip : Skip,
+            limit : Limit
+        }
       
-        axios.post('/api/product/products')
-            .then(response => {
-                if(response.data.success){
-                    console.log(response.data)
-                } else{
-                    alert("상품들을 가져오는데 실패하였습니다.")
-                }
-            })
-    
+        getProducts(body)
     }, [])
+
+    const getProducts = (body) => { axios.post('/api/product/products', body)
+    .then(response => {
+        if(response.data.success){
+            console.log(response.data)
+            if(body.loadMore){
+                setProducts([...Products, ...response.data.productInfo])
+
+            }else{
+                
+                setProducts(response.data.productInfo)
+            }
+            setPostSize(response.data.postSize)
+        } else{
+            alert("상품들을 가져오는데 실패하였습니다.")
+        }
+    })
+}
+
+    const renderCards = Products.map((product, index) => {
+        return <Col lg = {6} md = {8} xs = {24} key={index}>
+            <Card
+                
+                cover = {<ImageSlider images = {product.images} />} 
+            />
+                <Meta
+                    title = {product.title}
+                    description = {`$${product.price}`}
+                />
+
+            
+        </Col>
+    })
     
+
+    const loadMoreHandler = () => {
+        let skip = Skip + Limit
+
+        let body = {
+            skip : skip,
+            limit : Limit, 
+            loadMore : true
+        }
+        getProducts(body)
+        setSkip(skip)
+    }
+
+    const showFilteredResults = (filters) => {
+
+        let body = {
+            skip : 0,
+            limit : Limit,
+            filters : filters
+        }
+
+        getProducts(body)
+        setSkip(0)
+    }
+
+    const handleFilters = (filters, category) => {
+
+        const newFilters = {...Filters}
+
+        newFilters[category] = filters
+
+        showFilteredResults(newFilters)
+    }
+
     return (
-        <div>
-            LandingPage
+        <div style = {{width : '75%', margin : '3rem auto'}}>
+            
+            <div style = {{textAlign : 'center'}}>
+                <h2>Let's Travel Anywhere <Icon type = "rocket"/></h2>
+            </div>
+
+            <CheckBox list = {continents} handleFilters = {(filters) => handleFilters(filters, "continents")}/>
+
+            <Row gutter={[16, 16]}>
+                {renderCards}
+            </Row>
+            <br/>
+            {PostSize >= Limit && <div style={{ display : 'flex', justifyContent : 'center'}}>
+                <button onClick={loadMoreHandler}>더보기</button>
+            </div>}
+
+            
         </div>
+        
     )
 }
 
